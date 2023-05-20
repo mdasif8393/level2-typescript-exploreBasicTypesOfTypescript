ceab7eea1041af8310863933bb5522b2f015f0b5
unknown <md.asif.8393@gmail.com>
2023-05-20 23:33:37 +0600

    add inheritance

---
+++ src/module4/inheritance.ts
@@ -0,0 +1,39 @@
+class Parent {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    makeSleep(hours: number): string {
+        return `This ${this.name} is sleep for ${hours} hours`
+    }
+}
+
+class Student extends Parent {
+
+    constructor(name: string, age: number, address: string) {
+        super(name, age, address);
+    }
+}
+
+const student1 = new Student("Babul", 12, "Khulna");
+
+class Teacher extends Parent {
+    designation: string;
+
+    constructor(name: string, age: number, address: string, designation: string) {
+        super(name, age, address);
+        this.designation = designation
+    }
+
+
+    takeClasses(numberOfClass: number): string {
+        return `This ${this.name} will take ${numberOfClass}`
+    }
+}
+const teacher1 = new Teacher("Kabul", 43, "Dhaka", "Professor");
--- src/module4/object.ts
+++ src/module4/object.ts
@@ -1,23 +1,23 @@
-class Animal {
-    // public name: string;
-    // public species: string;
-    // public sound: string;
+// class Animal {
+//     // public name: string;
+//     // public species: string;
+//     // public sound: string;
 
-    //Parameter properties
-    constructor(public name: string, public species: string, public sound: string) {
-        // this.name = name;
-        // this.species = species;
-        // this.sound = sound;
-    }
+//     //Parameter properties
+//     constructor(public name: string, public species: string, public sound: string) {
+//         // this.name = name;
+//         // this.species = species;
+//         // this.sound = sound;
+//     }
 
-    public makeSound() {
-        console.log(`The ${this.name} dake ${this.sound} vabe`);
-    }
-}
+//     public makeSound() {
+//         console.log(`The ${this.name} dake ${this.sound} vabe`);
+//     }
+// }
 
-const dog = new Animal("Jarman SHepherd", "Dog", "Ghew Ghew");
-dog.name = "Kutta"
-dog.makeSound();
+// const dog = new Animal("Jarman SHepherd", "Dog", "Ghew Ghew");
+// dog.name = "Kutta"
+// dog.makeSound();
 
-const cat = new Animal("Persian", "Cat", "Mew Mew");
-cat.makeSound();
+// const cat = new Animal("Persian", "Cat", "Mew Mew");
+// cat.makeSound();
