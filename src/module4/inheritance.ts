ebc0298df66739ee6bc5f20ec897c69a21d1d9e5
unknown <md.asif.8393@gmail.com>
2023-05-22 09:43:03 +0600

    add type-guard

--- src/module4/inheritance.ts
+++ src/module4/inheritance.ts
@@ -1,39 +1,39 @@
-class Person {
-    name: string;
-    age: number;
-    address: string;
+// class Person {
+//     name: string;
+//     age: number;
+//     address: string;
 
-    constructor(name: string, age: number, address: string) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+//     constructor(name: string, age: number, address: string) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
 
-    makeSleep(hours: number): string {
-        return `This ${this.name} is sleep for ${hours} hours`
-    }
-}
+//     makeSleep(hours: number): string {
+//         return `This ${this.name} is sleep for ${hours} hours`
+//     }
+// }
 
-class Student extends Person {
+// class Student extends Person {
 
-    constructor(name: string, age: number, address: string) {
-        super(name, age, address);
-    }
-}
+//     constructor(name: string, age: number, address: string) {
+//         super(name, age, address);
+//     }
+// }
 
-const student1 = new Student("Babul", 12, "Khulna");
+// const student1 = new Student("Babul", 12, "Khulna");
 
-class Teacher extends Person {
-    designation: string;
+// class Teacher extends Person {
+//     designation: string;
 
-    constructor(name: string, age: number, address: string, designation: string) {
-        super(name, age, address);
-        this.designation = designation
-    }
+//     constructor(name: string, age: number, address: string, designation: string) {
+//         super(name, age, address);
+//         this.designation = designation
+//     }
 
 
-    takeClasses(numberOfClass: number): string {
-        return `This ${this.name} will take ${numberOfClass}`
-    }
-}
-const teacher1 = new Teacher("Kabul", 43, "Dhaka", "Professor");
+//     takeClasses(numberOfClass: number): string {
+//         return `This ${this.name} will take ${numberOfClass}`
+//     }
+// }
+// const teacher1 = new Teacher("Kabul", 43, "Dhaka", "Professor");
