ab23dfa0ec382d7a7cb6bdaf41c001d3f5317f11
unknown <md.asif.8393@gmail.com>
2023-05-23 16:31:11 +0600

    change codes

--- src/module4/getter-setter.ts
+++ src/module4/getter-setter.ts
@@ -1,45 +1,53 @@
-// class BankAccount{
-//     id: number;
-//     name: string;
-//     private _balance: number;
+class BankAccount{
+    id: number;
+    name: string;
+    private _balance: number;
 
-//     constructor(id: number, name: string, _balance: number){
-//         this.id = id;
-//         this.name = name;
-//         this._balance = _balance;
-//     }
+    constructor(id: number, name: string, _balance: number){
+        this.id = id;
+        this.name = name;
+        this._balance = _balance;
+    }
 
-//     //getter
-//     get balance(){
-//         return this._balance;
-//     }
+    private getTestBalance() : number{
+        return this._balance;
+    }
+    //get getTestBalance
+    get Test(): number{
+        return this.getTestBalance();
+    }
 
-//     //setter
-//     set deposit(amount: number){
-//         this._balance = this._balance + amount;
-//     }
+    //getter
+    get balance(){
+        return this._balance;
+    }
 
-//     getBalance() : number{
-//         return this._balance;
-//     }
-//     addDeposit(amount: number){
-//         this._balance = this._balance + amount;
-//     }
-// }
+    //setter
+    set deposit(amount: number){
+        this._balance = this._balance + amount;
+    }
 
-// class StudentAccount extends BankAccount{
-//     test(){
-        
-//     }
-// }
+    getBalance() : number{
+        return this._balance;
+    }
+    addDeposit(amount: number){
+        this._balance = this._balance + amount;
+    }
+}
+
+class StudentAccount extends BankAccount{
+    test(){
+        this.Test
+    }
+}
 
 
 
-// const myAccount = new BankAccount (444, "Persian", 100);
-// myAccount.addDeposit(50);
-// const balance = myAccount.getBalance();
-// console.log(balance);
-// const balance1 = myAccount.balance; //getter
-// console.log(balance1); 
-// myAccount.deposit = 100;
-// console.log(myAccount.balance); //setter
+const myAccount = new BankAccount (444, "Persian", 100);
+myAccount.addDeposit(50);
+const balance = myAccount.getBalance();
+console.log(balance);
+const balance1 = myAccount.balance; //getter
+console.log(balance1); 
+myAccount.deposit = 100;
+console.log(myAccount.balance); //setter
