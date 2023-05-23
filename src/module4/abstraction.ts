566a85f7bea353acdc4fe0899434f759f3cc9e9f
unknown <md.asif.8393@gmail.com>
2023-05-23 14:49:56 +0600

    change codes

--- src/module4/abstraction.ts
+++ src/module4/abstraction.ts
@@ -1,56 +1,56 @@
 //interface
-// interface Vehicle{
-//     startEngine(): void;
-//     stopEngine(): void;
-//     move(): void;
-// }
+interface Vehicle{
+    startEngine(): void;
+    stopEngine(): void;
+    move?(): void;
+}
 
-// class Car implements Vehicle{
+class Car implements Vehicle{
 
-//     constructor(
-//         public name: string,
-//         public brand: string,
-//         public model: number, 
-//     ){}
+    constructor(
+        public name: string,
+        public brand: string,
+        public model: number, 
+    ){}
 
-//     startEngine(): void {
-//         console.log("I am starting Engine....");
-//     }
-//     stopEngine(): void {
-//         console.log("I am stopping Engine");
-//     }
-//     move(): void {
-//         console.log("I am moving engine....");
-//     }
-// }
+    startEngine(): void {
+        console.log("I am starting Engine....");
+    }
+    stopEngine(): void {
+        console.log("I am stopping Engine");
+    }
+    move(): void {
+        console.log("I am moving engine....");
+    }
+}
 
-// const vehicle1 = new Car("Car", "Toyota", 2000);
+const vehicle1 = new Car("Car", "Toyota", 2000);
 
 
 
 
 //abstract class
 
-abstract class Vehicle{
-    constructor(
-        public name: string,
-        public brand: string,
-        public model: number, 
-    ){}
-    abstract startEngine(): void;
-    abstract stopEngine(): void;
-    move(): void {
-        console.log("I am moving..");
-    }; 
-}
+// abstract class Vehicle{
+//     constructor(
+//         public name: string,
+//         public brand: string,
+//         public model: number, 
+//     ){}
+//     abstract startEngine(): void;
+//     abstract stopEngine(): void;
+//     move(): void {
+//         console.log("I am moving..");
+//     }; 
+// }
 
-class Car extends Vehicle{
-    startEngine(): void {
-        console.log("Start engine");
-    }
-    stopEngine(): void {
-        console.log("Stop engine");
-    }
+// class Car extends Vehicle{
+//     startEngine(): void {
+//         console.log("Start engine");
+//     }
+//     stopEngine(): void {
+//         console.log("Stop engine");
+//     }
 
-}
+// }
  
