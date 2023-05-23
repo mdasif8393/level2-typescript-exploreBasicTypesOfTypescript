c08a56b4f856d7dfcd3518293585e531910d07c8
unknown <md.asif.8393@gmail.com>
2023-05-23 14:46:47 +0600

    change name vehicle to car

--- src/module4/abstraction.ts
+++ src/module4/abstraction.ts
@@ -1,62 +1,59 @@
 //interface
-// interface IVehicle{
-//     startEngine(): void;
-//     stopEngine(): void;
-//     move(): void;
-// }
+interface Vehicle{
+    startEngine(): void;
+    stopEngine(): void;
+    move(): void;
+}
 
-// class Vehicle implements IVehicle{
+class Car implements Vehicle{
 
-//     constructor(
-//         public name: string,
-//         public brand: string,
-//         public model: number, 
-//     ){}
+    constructor(
+        public name: string,
+        public brand: string,
+        public model: number, 
+    ){}
 
-//     startEngine(): void {
-//         console.log("I am starting Engine....");
-//     }
-//     stopEngine(): void {
-//         console.log("I am stopping Engine");
-//     }
-//     move(): void {
-//         console.log("I am moving engine....");
-//     }
-//     test() {
-//         console.log("I am for testing purpose");
-//     }
-// }
+    startEngine(): void {
+        console.log("I am starting Engine....");
+    }
+    stopEngine(): void {
+        console.log("I am stopping Engine");
+    }
+    move(): void {
+        console.log("I am moving engine....");
+    }
+}
 
-// const vehicle1 = new Vehicle("Car", "Toyota", 2000);
+const vehicle1 = new Car("Car", "Toyota", 2000);
 
 
 
 
 //abstract class
 
-abstract class Vehicle{
-    constructor(
-        public name: string,
-        public brand: string,
-        public model: number, 
-    ){}
-    abstract startEngine(): void;
-    abstract stopEngine(): void;
-    abstract move(): void; 
-    test() {
-        console.log("I am for testing purpose");
-    }
-}
+// abstract class Vehicle{
+//     constructor(
+//         public name: string,
+//         public brand: string,
+//         public model: number, 
+//     ){}
+//     abstract startEngine(): void;
+//     abstract stopEngine(): void;
+//     abstract move(): void; 
+//     test() {
+//         console.log("I am for testing purpose");
+//     }
+// }
 
-class Car extends Vehicle{
-    startEngine(): void {
-        console.log("Start engine");
-    }
-    stopEngine(): void {
-        console.log("Stop engine");
-    }
-    move(): void {
-        console.log("Moving engine");
-    }
-}
+// class Car extends Vehicle{
+//     startEngine(): void {
+//         console.log("Start engine");
+//     }
+//     stopEngine(): void {
+//         console.log("Stop engine");
+//     }
+//     move(): void {
+//         console.log("Moving");
+//     }
+// }
  
